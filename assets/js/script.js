4a7cb6b63999dff3ef272befb361d35756d88122
tristan-calleja <calleja.tristan@gmail.com>
2021-01-11 17:29:57 +0800

    second modal in progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,32 @@
-const cursor = document.querySelector('.cursor');
+// const cursor = document.querySelector('.cursor');
 
-document.addEventListener('mousemove', e => {
-    cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;")
-})
+// document.addEventListener('mousemove', e => {
+//     cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;")
+// })
 
-document.addEventListener('click', () => {
-    cursor.classList.add("expand");
+// document.addEventListener('click', () => {
+//     cursor.classList.add("expand");
 
-    setTimeout(() => {
-        cursor.classList.remove("expand");
-    }, 500)
-})
+//     setTimeout(() => {
+//         cursor.classList.remove("expand");
+//     }, 500)
+// })
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
